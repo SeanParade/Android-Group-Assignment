9abed093d204e22469dab7a4649c2c57943a63e1
seanparade <seanparade@gmail.com>
2017-12-10 14:13:27 -0500

    Initial project commit

---
+++ FlightBooker/app/src/main/java/com/gbc/flightbooker/ViewBookingsActivity.java
@@ -0,0 +1,13 @@
+package com.gbc.flightbooker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewBookingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bookings);
+    }
+}
